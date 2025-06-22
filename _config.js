21433212f4087a7fde8e0727be0c1d8cc05158dc
Virgotex <Virgotex15@gmail.com>
2025-06-22 16:45:55 +0300

    Fix test logic

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://virgotex:xpdlFreBLBtDGU85@virgotex.jqij3m0.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Virgotex',
+    development: 'mongodb+srv://virgotex:xpdlFreBLBtDGU85@virgotex.jqij3m0.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Virgotex',
+    test: 'mongodb+srv://virgotex:xpdlFreBLBtDGU85@virgotex.jqij3m0.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Virgotex',
 }
+
 module.exports = config;
--- server.js
+++ server.js
@@ -11,46 +11,29 @@ let image = require('./routes/image');
 // Initializing the app
 const app = express();
 
-// connecting the database
+// Connecting to MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
+        console.log('MongoDB error:', err);
+    } else {
+        console.log(`✅ Connected to MongoDB: ${MONGODB_URI}`);
     }
 });
 
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
-
-
-// View Engine
+// Set view engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
-app.use(express.json())
+// Body parser
+app.use(express.json());
 
-
+// Routes
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
-
-
+// ✅ Don't start the server here
 module.exports = app;
